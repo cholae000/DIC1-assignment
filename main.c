7eeb97f961f31ae6067e179f06afd6647d56285d
cholae000 <87842631+cholae000@users.noreply.github.com>
2023-03-19 15:46:37 +0630

    assignment

---
+++ main.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+#include "time.h"
+#include "assignment10.h"
+
+int main(){
+    space_counter();
+    loadingAllDataFromFile();
+    printingAllData();
+    welcome();
+
+    return 0;
+}
+
